13fe965b2fc96dbae360ed71640bab5a213d78cf
unknown <florian.vassiliadi1@gmail.com>
2016-02-14 18:26:22 +0100

    -Enfilage defilage ok

---
+++ FileThreadUnsafe.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tp4
+{
+    public class FileThreadUnsafe<T>
+    {
+        private T[] tab; private int tete, queue;
+        public FileThreadUnsafe(int taille)
+        {
+            tab = new T[taille]; Init();
+        }
+        private int Suivant(int i) {
+            return (i + 1) % tab.Length;
+        }
+        
+        private void Init() { tete = queue = -1; }
+        public void Enfiler(T element) {
+            if (Pleine())
+                throw new ExceptionFilePleine();
+            else if (Vide())
+                tab[queue = tete = 0] = element;
+            else
+            {
+                tab[queue = Suivant(queue)] = element;
+            }
+        }
+
+        public void Defiler()
+        {
+            if (Vide())
+                throw new ExceptionFileVide();
+            else if (tete == queue)
+                tete = queue = -1;
+            else
+                tete = tete + 1;
+        }
+        public bool Vide()
+        { 
+            bool result = false;
+            if (tete == -1)
+                result = true;
+            return result;
+        }
+        public bool Pleine()
+        {
+            bool result = false;
+            if(Suivant(queue)==tete)
+            {
+                result = true;
+            }
+            return result;
+        }
+        public int NbElements()
+        {
+            if (Vide()) return 0; else if (tete <= queue) return queue - tete + 1; else return tab.Length + queue - tete + 1;
+        }
+        //public T Premier()
+        //{ // TODO 
+        //    T result = new T();
+        //    return result;
+        //}
+        public void Afficher()
+        {
+            Console.WriteLine();
+            int i = tete;
+            while(i<=queue)
+            {
+                int r = i;
+                Console.Write(" "+tab[i]);
+                i++;
+            }
+        }
+    }
+    public class ExceptionFileVide : Exception { }
+    public class ExceptionFilePleine : Exception { }
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tp4;
+
+namespace tp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileThreadUnsafe<String> f = new tp4.FileThreadUnsafe<String>(4);
+            f.Enfiler("Florian");
+            f.Afficher();
+
+            f.Enfiler("Germain");
+            f.Afficher();
+
+            f.Enfiler("Alex");
+            f.Afficher();
+
+            f.Enfiler("Math");
+            f.Afficher();
+
+            f.Defiler();
+            f.Afficher();
+
+            f.Defiler();
+            f.Afficher();
+            //int cpt = 0;
+            //while(cpt<100)
+            //{
+            //    f.Enfiler(cpt);
+            //    f.Afficher();
+            //    cpt++;
+            //    System.Threading.Thread.Sleep(1000);
+            //}
+        }
+        
+    }
+}
